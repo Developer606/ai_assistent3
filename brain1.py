ecd1d9293baf2f6085d59b69c83a7673b609eeca
Developer606 <131393944+Developer606@users.noreply.github.com>
2023-04-21 00:17:09 +0530

    Update and rename README.md to brain1.py

---
+++ brain1.py
@@ -0,0 +1,26 @@
+import openai
+from speek import say
+openai.api_key = ""
+def pro(a):
+    prompt = a
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=60,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return response.choices[0].text
+while True:
+    say(pro(input("enter: ")))
+
+# response = openai.Completion.create(
+#   model="text-davinci-003",
+#   prompt="how are you",
+#   temperature=0.51,
+#   max_tokens=256,
+#   top_p=1,
+#   frequency_penalty=0,
+#   presence_penalty=0
+# )
